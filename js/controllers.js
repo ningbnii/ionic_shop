72323b63b6bad424a6d3f9cf693d8d1a5bb5a923
ningbnii <296720094@qq.com>
2016-08-25 19:02:57 +0800

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,16 @@
+angular.module('starter.controllers', [])
+
+.controller('IndexCtrl', function($scope) {})
+
+.controller('SearchCtrl', function($scope) {
+
+})
+
+.controller('CenterCtrl', function($scope) {
+
+})
+
+/* 购物车 */
+.controller('CartCtrl', function($scope){
+
+})
